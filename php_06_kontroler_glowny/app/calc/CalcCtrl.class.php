fbde1f9f2a8ab1d496acd482f5ce1a0dd2b549fb
katarzynatarnowska <tk.tarnowska.kasia@gmail.com>
2022-03-29 01:01:46 +0200

    php_kontroler_glowny

---
+++ php_06_kontroler_glowny/app/calc/CalcCtrl.class.php
@@ -0,0 +1,131 @@
+<?php
+// W skrypcie definicji kontrolera nie trzeba dołączać problematycznego skryptu config.php,
+// ponieważ będzie on użyty w miejscach, gdzie config.php zostanie już wywołany.
+
+require_once $conf->root_path.'/lib/smarty/Smarty.class.php';
+require_once $conf->root_path.'/lib/Messages.class.php';
+require_once $conf->root_path.'/app/calc/CalcForm.class.php';
+require_once $conf->root_path.'/app/calc/CalcResult.class.php';
+
+/** Kontroler kalkulatora
+ * @author Przemysław Kudłacik
+ *
+ */
+class CalcCtrl {
+
+	private $msgs;   //wiadomości dla widoku
+	private $infos;  //informacje dla widoku
+	private $form;   //dane formularza (do obliczeń i dla widoku)
+	private $result; //inne dane dla widoku
+	private $hide_intro; //zmienna informująca o tym czy schować intro
+
+	/** 
+	 * Konstruktor - inicjalizacja właściwości
+	 */
+	public function __construct(){
+		//stworzenie potrzebnych obiektów
+		$this->msgs = new Messages();
+		$this->form = new CalcForm();
+		$this->result = new CalcResult();
+		$this->hide_intro = false;
+	}
+	
+	/** 
+	 * Pobranie parametrów
+	 */
+	public function getParams(){
+		$this->form->kwota = isset($_REQUEST ['kwota']) ? $_REQUEST ['kwota'] : null;
+		$this->form->raty = isset($_REQUEST ['raty']) ? $_REQUEST ['raty'] : null;
+		$this->form->procent = isset($_REQUEST ['procent']) ? $_REQUEST ['procent'] : null;
+	}
+	
+	/** 
+	 * Walidacja parametrów
+	 * @return true jeśli brak błedów, false w przeciwnym wypadku 
+	 */
+	public function validate() {
+		// sprawdzenie, czy parametry zostały przekazane
+		if (! (isset ( $this->form->kwota ) && isset ( $this->form->raty ) && isset ( $this->form->procent ))) {
+			// sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+			return false;
+		} else { 
+			$this->hide_intro = true; //przyszły pola formularza - schowaj wstęp
+		}
+		
+		// sprawdzenie, czy potrzebne wartości zostały przekazane
+		if ($this->form->kwota == "") {
+			$this->msgs->addError('Nie podano kwoty');
+		}
+		if ($this->form->raty == "") {
+			$this->msgs->addError('Nie podano okresu');
+		}
+		if ($this->form->procent == "") {
+			$this->msgs->addError('Nie podano oprocentowania');
+		}
+		
+		// nie ma sensu walidować dalej gdy brak parametrów
+		if (! $this->msgs->isError()) {
+			
+			// sprawdzenie, czy $x i $y są liczbami całkowitymi
+			if (! is_numeric ( $this->form->kwota )) {
+				$this->msgs->addError('Pierwsza wartość nie jest liczbą');
+			}
+			
+			if (! is_numeric ( $this->form->raty )) {
+				$this->msgs->addError('Druga wartość nie jest liczbą');
+			}
+
+			if (! is_numeric ( $this->form->procent )) {
+				$this->msgs->addError('Trzecia wartość nie jest liczbą ');
+			}
+		}
+		
+		return ! $this->msgs->isError();
+	}
+	
+	/** 
+	 * Pobranie wartości, walidacja, obliczenie i wyświetlenie
+	 */
+	public function process(){
+
+		$this->getparams();
+		
+		if ($this->validate()) {
+				
+			//konwersja parametrów na int
+			$this->form->kwota = intval($this->form->kwota);
+			$this->form->raty = intval($this->form->raty);
+			$this->form->procent = intval($this->form->procent);
+				
+			//wykonanie operacji
+			$this->result->result = ((($this->form->procent/100)* $this->form->kwota* $this->form->raty)+$this->form->kwota)/($this->form->raty*12);
+			
+			$this->msgs->addInfo('Wykonano obliczenia.');
+		}
+		
+		$this->generateView();
+	}
+	
+	
+	/**
+	 * Wygenerowanie widoku
+	 */
+	public function generateView(){
+		global $conf;
+		
+		$smarty = new Smarty();
+		$smarty->assign('conf',$conf);
+		
+		$smarty->assign('page_title','Zadanie- kontroler główny');
+		
+		$smarty->assign('page_header','Kontroler główny');
+				
+		$smarty->assign('hide_intro',$this->hide_intro);
+		
+		$smarty->assign('msgs',$this->msgs);
+		$smarty->assign('form',$this->form);
+		$smarty->assign('res',$this->result);
+		
+		$smarty->display($conf->root_path.'/app/calc/CalcView.html');
+	}
+}
---
+++ php_06_kontroler_glowny/templates_c/369cc35844952f6dad02efcec67def5f67bdffc9_0.file.main.html.php
@@ -0,0 +1,145 @@
+<?php
+/* Smarty version 3.1.30, created on 2022-03-29 00:56:28
+  from "/Applications/XAMPP/xamppfiles/htdocs/php_06_kontroler_glowny/templates/main.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_62423d1c637298_96404368',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '369cc35844952f6dad02efcec67def5f67bdffc9' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/php_06_kontroler_glowny/templates/main.html',
+      1 => 1648508186,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_62423d1c637298_96404368 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (($tmp = @$_smarty_tpl->tpl_vars['page_description']->value)===null||$tmp==='' ? "Opis domyślny" : $tmp);?>
+">
+
+    <title><?php echo (($tmp = @$_smarty_tpl->tpl_vars['page_title']->value)===null||$tmp==='' ? "Zadanie- kontroler główny" : $tmp);?>
+</title>
+
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
+
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/main-grid-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/main-grid.css">
+    <!--<![endif]-->
+  
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/layouts/marketing-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/layouts/marketing.css">
+    <!--<![endif]-->
+
+    <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/style.css">
+<?php if ($_smarty_tpl->tpl_vars['hide_intro']->value) {?>
+    <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/css/style_hide_intro.css">
+<?php }?>
+	
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/js/jquery.min.js"><?php echo '</script'; ?>
+>
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['conf']->value->app_url;?>
+/js/softscroll.js"><?php echo '</script'; ?>
+>
+</head>
+<body>
+
+<div id="app_top" class="header">
+    <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href=""><?php echo (($tmp = @$_smarty_tpl->tpl_vars['page_title']->value)===null||$tmp==='' ? "Zadanie- kontroler główn" : $tmp);?>
+</a>
+        <ul>
+            <li class="pure-menu-selected"><a href="#app_top">Strona tytułowa</a></li>
+            <li><a href="#app_content">Kalkulator</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="splash-container">
+    <div class="splash">
+
+     <br></br> <br></br>
+
+        <p>
+            <a href="#app_content" class="pure-button pure-button-primary">Kliknij aby przejść do formularza</a>
+        </p>
+    </div>
+</div>
+
+<div class="content-wrapper">
+
+    <div id="app_content" class="content">
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_71519552362423d1c635ca2_84852520', 'content');
+?>
+
+
+    </div>
+
+    <div class="footer l-box is-center">
+		<p>
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_49669288262423d1c636ae1_12502528', 'footer');
+?>
+
+		</p>
+        <p>Widok oparty na stylach i szablonie <a href="http://purecss.io/" target="_blank">Pure CSS Yahoo!</a></p>
+    </div>
+
+</div>
+
+
+</body>
+</html>
+<?php }
+/* {block 'content'} */
+class Block_71519552362423d1c635ca2_84852520 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+/* {block 'footer'} */
+class Block_49669288262423d1c636ae1_12502528 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść stopki .... <?php
+}
+}
+/* {/block 'footer'} */
+}
